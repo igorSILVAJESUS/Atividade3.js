9c9741a4e09245f98e65e0533edb0ed438077bc3
igorSILVAJESUS <igorsilva@colegiopaulodetarso.com.br>
2025-05-26 16:18:56 +0000

    kkkkk

---
+++ execicio1.js
@@ -0,0 +1,3 @@
+const nome = "igor jesus silva", idade = 17;
+const dias = idade * 365, minutos = dias * 1440, segundos = minutos * 60;
+console.log(`Nome: ${nome}\nIdade: ${idade} anos\nDias: ${dias}\nMinutos: ${minutos}\nSegundos: ${segundos}`);
